c699c11f0ee9adce41ca2b779133a41607723dfe
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2025-02-28 14:30:32 +0200

    trying 3

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import HeroSection from "./_components/hero-section";
 
-export default function Home() {
+export default function page() {
   return (
     <div className="w-full h-auto">
       <HeroSection />
